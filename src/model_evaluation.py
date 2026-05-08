2d787d12f1b410c574f44dbfa8487972dffb8f2d
Prem Mote <moteprem4@gmail.com>
2026-05-09 02:26:50 +0530

    evaluation code changed

--- src/model_evaluation.py
+++ src/model_evaluation.py
@@ -126,7 +126,7 @@ def main():
             live.log_metric('accuracy', accuracy_score(y_test, y_test))
             live.log_metric('precision', precision_score(y_test, y_test))
             live.log_metric('recall', recall_score(y_test, y_test))
-
+            live.log_metric('auc', roc_auc_score(y_test, y_test))
             live.log_params(params)
         
         save_metrics(metrics, 'reports/metrics.json')
